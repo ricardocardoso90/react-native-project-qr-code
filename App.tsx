2c39cf6054191fcb98914a5e575d3ad6a67b69c3
ricardocardoso90 <ricardomanolo90@gmail.com>
2024-11-15 13:29:27 -0300

    update commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, View, Modal } from 'react-native';
+import { CameraView, useCameraPermissions } from 'expo-camera';
 
 export default function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
   return (
     <View style={styles.container}>
       <Button title='Ler QRCode' />
+
+      <Modal visible={modalIsVisible}>
+        <CameraView />
+      </Modal>
     </View>
   );
 }
@@ -14,6 +21,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center', 
+    justifyContent: 'center',
   },
 });
